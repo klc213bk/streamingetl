8a410159b8e3440e2e2736e043575afb94f6e167
feib <feib@localhost>
2021-05-01 02:18:42 +0800

    initial

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # streamingetl
+
+# list topic
+$ ./bin/kafka-topics.sh --zookeeper localhost:2181 --list
+
+# delete topic
+$ ./bin/kafka-topics.sh --zookeeper localhost:2181 --delete --topic remove-me
--- bin/start-logminer.sh
+++ bin/start-logminer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-KAFKA_HOME=/home/oracle/kafka_2.13-2.7.0
-APP_HOME=/home/oracle/gitrepository/transglobe/streamingetl-kafka
+#KAFKA_HOME=/home/oracle/kafka_2.13-2.7.0
+#APP_HOME=/home/oracle/gitrepository/transglobe/streamingetl
 
-#KAFKA_HOME=/home/feib/kafka_2.13-2.7.0
-#APP_HOME=/home/feib/gitrepository/transglobe/streamingetl-kafka
+KAFKA_HOME=/home/feib/kafka_2.13-2.7.0
+APP_HOME=/home/feib/gitrepository/transglobe/streamingetl
 
 ${KAFKA_HOME}/bin/connect-standalone.sh ${APP_HOME}/config/logminer_connect-standalone.properties ${APP_HOME}/config/OracleSourceConnector.properties
 
