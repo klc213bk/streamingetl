9fa712ba8f964e3e1138cc5cb15b6e96a4d45db3
feib <feib@localhost>
2021-05-02 01:52:43 +0800

    initial

--- ignite/bin/start-ignite-pcr420669.sh
+++ ignite/bin/start-ignite-pcr420669.sh
@@ -3,6 +3,7 @@
 #IGNITE_HOME=/home/oracle/apache-ignite-2.9.1-bin
 IGNITE_HOME=/home/feib/apache-ignite-2.9.1-bin
 
+APP_HOME=/home/feib/gitrepository/transglobe/streamingetl
 
-${IGNITE_HOME}/bin/ignite.sh ../config/pcr420669-ignite.xml
+${IGNITE_HOME}/bin/ignite.sh ${APP_HOME}/ignite/config/pcr420669-ignite.xml
 
