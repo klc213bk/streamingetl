f560884d40e6d364662f155e28ad1d88537b410f
klc213bk <klc213bk@gmail.com>
2021-05-12 14:30:50 +0800

    initial

---
+++ env-uat/bin/start-kafka-servers.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+KAFKA_HOME=/home/kafka/kafka_2.13-2.7.0
+
+cd ${KAFKA_HOME}
+
+gnome-terminal --title zookeeper --tab -e  "/bin/bash -c './bin/zookeeper-server-start.sh ./config/zookeeper.properties; bash'"
+
+echo "Please Wait !!!"
+
+sleep 3s
+
+gnome-terminal --title kafka --tab -e  "/bin/bash -c './bin/kafka-server-start.sh ./config/server.properties; bash'"
+
+
---
+++ env-uat/bin/start-logminer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+KAFKA_HOME=/home/kafka/kafka_2.13-2.7.0
+APP_HOME=/home/kafka/streamingetl/env-uat
+
+${KAFKA_HOME}/bin/connect-standalone.sh ${APP_HOME}/config/logminer_connect-standalone.properties ${APP_HOME}/config/OracleSourceConnector.properties
+
