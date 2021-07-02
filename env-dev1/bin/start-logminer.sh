b1cc043783be96f4a5729100022e0b38b22f423b
steven <steven@mylinux>
2021-07-02 18:01:55 +0800

    initial

--- env-dev1/bin/start-logminer.sh
+++ env-dev1/bin/start-logminer.sh
@@ -3,4 +3,4 @@
 KAFKA_HOME=/home/steven/kafka_2.13-2.7.0
 STREAMINGETL_HOME=/home/steven/gitrepo/transglobe/streamingetl
 
-${KAFKA_HOME}/bin/connect-standalone.sh ${APP_HOME}/env-dev1/config/logminer_connect-standalone.properties ${APP_HOME}/config/OracleSourceConnector.properties
+${KAFKA_HOME}/bin/connect-standalone.sh ${STREAMINGETL_HOME}/env-dev1/config/logminer_connect-standalone.properties ${APP_HOME}/config/OracleSourceConnector.properties
