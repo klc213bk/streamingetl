6088d329d2db97f91238bcb548cecf8bcd9a8a80
feib <feib@localhost>
2021-07-03 21:17:37 +0800

    inital

--- env-dev1/bin/create-kafka-topics.sh
+++ env-dev1/bin/create-kafka-topics.sh
@@ -11,4 +11,4 @@ cd ${KAFKA_HOME}
 ./bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ebao.cdc.test_t_insured_list_log.0
 ./bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ebao.cdc.test_t_contract_bene_log.0
 ./bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ebao.cdc.test_t_address.0
-./bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ebao.cdc.t_streaming_etl_health_cdc.0
+./bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic tglminer.cdc.t_streaming_etl_health_cdc.0
--- env-dev2/bin/start-ignite-pcr420669.sh
+++ env-dev2/bin/start-ignite-pcr420669.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 IGNITE_HOME=/home/feib/apache-ignite-2.9.1-bin
+STREAMINGETL_HOME=/home/feib/gitrepository/transglobe/streamingetl
 
-${IGNITE_HOME}/bin/ignite.sh ${IGNITE_HOME}/config/pcr420669/ignite-dev2.xml
+${IGNITE_HOME}/bin/ignite.sh ${STREAMINGETL_HOME}/env-dev2/config/ignite-pcr420669.xml
 
