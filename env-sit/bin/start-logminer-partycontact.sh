20db96a1bc2be7187616be7ce9dceb608dc4b25a
steven <steven@mylinux>
2021-07-27 15:12:40 +0800

    initial

--- env-sit/bin/start-logminer-partycontact.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-KAFKA_HOME=/home/kafka/kafka_2.13-2.7.0
-STREAMINGETL_HOME=/home/kafka/streamingetl
-
-${KAFKA_HOME}/bin/connect-standalone.sh ${STREAMINGETL_HOME}/env-sit/config/logminer_connect-standalone-partycontact.properties ${STREAMINGETL_HOME}/env-sit/config/OracleSourceConnector-partycontact.properties
