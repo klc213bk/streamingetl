88cc21c0ce1a14caccdb67527206bb381ce3cae3
steven <steven@mylinux>
2021-08-06 16:34:58 +0800

    # WARNING: head commit changed in the meantime
    initial

--- env-dev1/bin/start-init-streaming.sh
+++ env-dev1/bin/start-init-streaming.sh
@@ -5,4 +5,4 @@ KAFKA_HOME=/home/steven/kafka_2.13-2.7.0
 
 java -cp "${STREAMINGETL_HOME}/lib/streamingetl-common-1.0.jar:${STREAMINGETL_HOME}/lib/*" -Dprofile.active=env-dev1 com.transglobe.streamingetl.common.app.InitStreamingApp
 
-${KAFKA_HOME}/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ebao.cdc.t_logminer_scn.0
+${KAFKA_HOME}/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic EBAOUAT1.TGLMINER.T_LOGMINER_SCN
